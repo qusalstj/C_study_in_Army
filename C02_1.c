7751990340cc9259a793bf9d6edcdd4ff109889f
qusalstj <127468242+qusalstj@users.noreply.github.com>
2024-01-18 20:34:26 +0900

    Add files via upload

---
+++ C02_1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("º¯¹Î¼­\nº¯ ¹Î ¼­\nº¯  ¹Î  ¼­");
+	return 0;
+}
---
+++ C02_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("변민서\n");
+	printf("서울특별시 은평구 통일로 866-1\n");
+	printf("010-9705-0408");
+	return 0;
+}
