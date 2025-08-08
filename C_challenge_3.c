21d73585d599b8a1dd90cb7ab974c25ca26303b0
qusalstj <127468242+qusalstj@users.noreply.github.com>
2025-08-08 19:06:50 +0900

    C_challenge_3.c

--- C_challenge_3.c
+++ C_challenge_3.c
@@ -1,4 +1,4 @@
-int gdd(int a, int b){
+int gcd(int a, int b){
     int result =1;
     for(int i=1;i<=b;i++){
         if((a%i==0)&&(b%i==0))
@@ -10,9 +10,9 @@ int main(void){
     int num1,num2;
     scanf("%d %d",&num1,&num2);
     if(num1>num2)
-        gdd(num1,num2);
+        printf("%d",gdd(num1,num2));
     else if(num1<num2)
-        gdd(num2,num1);
+        printf("%d",gdd(num2,num1));
     else
         printf("nope");
 }
