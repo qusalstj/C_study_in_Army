fa3d7a15b67518c3cde55135a0def1d9e4542dc2
qusalstj <127468242+qusalstj@users.noreply.github.com>
2025-08-11 18:34:36 +0900

    C_11_2_2.c

---
+++ C_11_2_2.c
@@ -0,0 +1,13 @@
+int main(void){
+    char str[20];
+    int len=0;
+    scanf("%s",str);
+    while(str[len]!='\0'){
+        len++;}
+    for(int i=0;i<len/2;i++){
+        char box;
+        box=str[(len-i)-1];
+        str[(len-i)-1]=str[i];
+        str[i]=box;}
+    printf("%s\n",str);
+}
